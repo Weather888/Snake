bc4a2a7a4ad118d4d16eb3db1a4667ae54c67693
Weather888 <83582510+Weather888@users.noreply.github.com>
2022-02-22 17:42:21 +0300

    v3

--- Snake/Form1.cs
+++ Snake/Form1.cs
@@ -29,6 +29,7 @@ namespace Snake
         public int[] dy = { -1, 0, 1, 0 };//массивы смещений
         public int[] dx = { 0, 1, 0, -1 };
         Pair Head, Tail;
+        Random Rnd = new Random(DateTime.Now.Millisecond);
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
@@ -59,8 +60,8 @@ namespace Snake
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Width = width * k;
-            Height = height * k;
+            Width = (width + 1) * k + 20;
+            Height = (height + 1) * k + 40;
             Head = new Pair() { x = width / 2, y = height / 2 };
             gr = Graphics.FromImage(field);
             gr.Clear(Color.Black);
@@ -69,9 +70,11 @@ namespace Snake
             {
                 int x = width / 2, y = height / 2 + i;
                 q.Enqueue(new Pair() { x = x, y = y });//добавляем элемент
+                Map[x, y] = 1;//запоминаем точку на карте
                 gr.FillRectangle(Brushes.Lime, x * k, y * k, k, k);//клетка
                 gr.DrawRectangle(Pens.Black, x * k, y * k, k, k);//границы
             }
+            AddFood();
             pictureBox1.Image = field;//привязываем картинку, которую мы сделали, и запускаем
             timer1.Start();
         }
@@ -81,7 +84,18 @@ namespace Snake
         {
             InitializeComponent();
         }
-
+        public void AddFood()
+        {
+            int x, y;
+            do
+            {
+                x = 1 + Rnd.Next(height - 1);
+                y = 1 + Rnd.Next(width - 1);
+            } while (Map[x, y] != 0);
+            Map[x, y] = 2;
+            gr.FillEllipse(Brushes.Yellow, x * k + 2, y * k + 2, k - 4, k - 4);
+            pictureBox1.Image = field;
+        }
         private void timer1_Tick(object sender, EventArgs e)
         {
             Head.x += dx[Direction]; Head.y += dy[Direction];//изменится голова змейки
@@ -89,11 +103,41 @@ namespace Snake
             if (Head.y < 0) Head.y = width;
             if (Head.x > width) Head.x = 0;
             if (Head.y > height) Head.y = 0;
-            q.Enqueue(new Pair() { x = Head.x, y = Head.y });//в очередь добавляем новую координату змейки
-            gr.FillRectangle(Brushes.Lime, Head.x * k, Head.y * k, k, k);//рисуем змейку на новой координате
-            gr.DrawRectangle(Pens.Black, Head.x * k, Head.y * k, k, k);
-            Tail = q.Dequeue();//хвост
-            gr.FillRectangle(Brushes.Black, Tail.x * k, Tail.y * k, k, k);//зарисовываем черным
+            if (Map[Head.x, Head.y] == 1)//если клетка занята останавливаем таймер
+             // q.Enqueue(new Pair() { x = Head.x, y = Head.y });//в очередь добавляем новую координату змейки
+             //gr.FillRectangle(Brushes.Lime, Head.x * k, Head.y * k, k, k);//рисуем змейку на новой координате
+             //gr.DrawRectangle(Pens.Black, Head.x * k, Head.y * k, k, k);
+             //Tail = q.Dequeue();//хвост
+             //gr.FillRectangle(Brushes.Black, Tail.x * k, Tail.y * k, k, k);//зарисовываем черным
+             //pictureBox1.Image = field;
+             
+            {
+                gr.FillRectangle(Brushes.OrangeRed, Head.x * k, Head.y * k, k, k);
+                gr.DrawRectangle(Pens.Black, Head.x * k, Head.y * k, k, k);
+                timer1.Stop();
+            }
+            else
+            {
+                if (Map[Head.x, Head.y] == 2)
+                {
+                    AddFood();
+                    Food += 3;
+                }
+                q.Enqueue(new Pair() { x = Head.x, y = Head.y });
+                Map[Head.x, Head.y] = 1;
+                gr.FillRectangle(Brushes.Lime, Head.x * k, Head.y * k, k, k);
+                gr.DrawRectangle(Pens.Black, Head.x * k, Head.y * k, k, k);
+                if (Food > 0)
+                {
+                    Food--;
+                }
+                else
+                {
+                    Tail = q.Dequeue();
+                    Map[Tail.x, Tail.y] = 0;// удаляем
+                    gr.FillRectangle(Brushes.Black, Tail.x * k, Tail.y * k, k, k);
+                }
+            }
             pictureBox1.Image = field;
         }
     }
